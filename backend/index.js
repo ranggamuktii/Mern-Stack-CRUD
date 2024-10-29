571c8fa4888deb3e8bfe28bd81d1a595e4e88e27
Rangga Mukti <r4n994mukti@gmail.com>
2024-10-29 08:22:03 +0700

    Import UserRoute kedalam index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import UserRoute from './routes/UserRoute.js';
 
 const app = express();
 const port = 5000;
@@ -16,5 +17,6 @@ db.once('open', () => console.log('Database Connected...'));
 
 app.use(cors());
 app.use(express.json());
+app.use(UserRoute);
 
 app.listen(port, () => console.log(`Server up and running in ${port}...`));

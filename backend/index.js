ea1142054642a32776a9bb0babb2a321ae888884
Rangga Mukti <r4n994mukti@gmail.com>
2024-10-29 08:01:18 +0700

    Setup server dan connect pada database mongodb

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+const port = 5000;
+
+mongoose
+  .connect('mongodb://localhost:27017/merncrud_db')
+  .then(() => console.log('Terhubung ke MongoDB'))
+  .catch((error) => console.error('Gagal terhubung ke MongoDB:', error));
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Database Connected...'));
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => console.log(`Server up and running in ${port}...`));

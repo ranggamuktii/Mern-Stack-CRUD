121e7ee7e7e9685cbdc96ae6983678154e0b76f3
Rangga Mukti <r4n994mukti@gmail.com>
2024-10-29 11:49:31 +0700

    Menambahkan controller saveUser

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -17,3 +17,13 @@ export const getUserById = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
+
+export const saveUser = async (req, res) => {
+  const user = new User(req.body);
+  try {
+    const inserteduser = await user.save();
+    res.status(201).json(inserteduser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getUsers, getUserById } from '../controllers/UserController.js';
+import { getUsers, getUserById, saveUser } from '../controllers/UserController.js';
 
 const router = express.Router();
 
 router.get('/users', getUsers);
 router.get('/users/:id', getUserById);
+router.post('/users', saveUser);
 
 export default router;
